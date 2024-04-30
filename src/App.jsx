41df9f6a2a41d24e21acf0584b96ef3707e18ad3
SUDHIR KUMAR <167057380+Sudhir-Kumar-Gamersback@users.noreply.github.com>
2024-04-30 12:01:52 +0530

    layout setter

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SearchAppBar from './components/Navbar'
+const App = () => {
+  return (
+    <div>
+      <SearchAppBar />
+    </div>
+  )
+}
+
+export default App
